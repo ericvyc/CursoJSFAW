1320a773f1c6eb36cf8195139297318232d4a2d7
ericvyc <eric_vilar2007@hotmail.com>
2015-10-27 23:42:17 -0200

    Dependencia do pom.xml e aula de outros escopos de bean.

--- src/main/java/br/com/cursojsf/javaee/visao/gestaoprodutos/GestaoProdutosBean.java
+++ src/main/java/br/com/cursojsf/javaee/visao/gestaoprodutos/GestaoProdutosBean.java
@@ -1,20 +1,41 @@
 package br.com.cursojsf.javaee.visao.gestaoprodutos;
 
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.RequestScoped;
 
 import br.com.cursojsf.javaee.dominio.Produto;
 
 @ManagedBean
-@SessionScoped
-public class GestaoProdutosBean {
+@RequestScoped
+public class GestaoProdutosBean implements Serializable{
+
+	private static final long serialVersionUID = -62854982190177074L;
 
 	private Produto produto;
 
 	private List<Produto> produtos;
+
+	private SimpleDateFormat sdf = new SimpleDateFormat();
+
+	@PostConstruct
+	public void inicializa(){
+		Date dt1 = new Date();
+		System.out.println("Inicializou: " + dt1.getHours() + ":" + dt1.getMinutes() + ":" + dt1.getSeconds());
+	}
+
+	@PreDestroy
+	public void finaliza(){
+		Date dt1 = new Date();
+		System.out.println("Terminou: " + dt1.getHours() + ":" + dt1.getMinutes() + ":" + dt1.getSeconds());
+	}
 
 	public void incluir(){
 		getProdutos().add(getProduto());
